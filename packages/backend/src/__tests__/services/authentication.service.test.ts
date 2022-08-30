71c399ece54b67fe1d21dfb81ed59348551f2d3a
KishenKumarrrrr <kishen1198@gmail.com>
2022-08-30 21:59:55 +0800

    feat(backend): add authentication service

---
+++ packages/backend/src/__tests__/services/authentication.service.test.ts
@@ -0,0 +1,48 @@
+import { mockDeep, mockReset } from 'jest-mock-extended';
+import bcrypt from 'bcrypt';
+import { PrismaClient, User } from '.prisma/client';
+import authenticationService from '../../services/authentication.service';
+
+const prismaMock = mockDeep<PrismaClient>();
+
+beforeEach(() => {
+  mockReset(prismaMock);
+});
+
+
+describe('authentication.service tests', () => {
+  test('ValidUser_ReturnsTrue', async () => {
+    const prismaResponseObject : User = {
+      user_id : 1,
+      user_email : 'testUser@test.com',
+      user_password_hash : bcrypt.hashSync('testPassword', 10),
+    };
+    prismaMock.user.findUnique.mockResolvedValueOnce(prismaResponseObject);
+    await expect(authenticationService.validateUser('testUser@test.com', 'testPassword', prismaMock)).resolves.toEqual(true);
+  });
+
+  test('InvalidUser_NoSuchUser_ReturnsFalse', async () => {
+    prismaMock.user.findUnique.mockResolvedValueOnce(null);
+    await expect(authenticationService.validateUser('testUser@test.com', 'testPassword', prismaMock)).resolves.toEqual(false);
+  });
+
+  test('InvalidUser_InvalidCredentials_ReturnsFalse', async () => {
+    const prismaResponseObject : User = {
+      user_id : 1,
+      user_email : 'testUser@test.com',
+      user_password_hash : bcrypt.hashSync('testPassword', 10),
+    };
+    prismaMock.user.findUnique.mockResolvedValueOnce(prismaResponseObject);
+    await expect(authenticationService.validateUser('testUser@test.com', 'wrongTestPassword', prismaMock)).resolves.toEqual(false);
+  });
+
+  test('InvalidUser_UserHasNoPasswordSet_ReturnsFalse', async () => {
+    const prismaResponseObject : User = {
+      user_id : 1,
+      user_email : 'testUser@test.com',
+      user_password_hash : null,
+    };
+    prismaMock.user.findUnique.mockResolvedValueOnce(prismaResponseObject);
+    await expect(authenticationService.validateUser('testUser@test.com', 'testPassword', prismaMock)).resolves.toEqual(false);
+  });
+});
---
+++ packages/backend/src/services/authentication.service.ts
@@ -0,0 +1,25 @@
+import { PrismaClient } from '.prisma/client';
+import bcrypt from 'bcrypt';
+
+
+async function validateUser(userEmail: string, userPassword: string, prisma: PrismaClient) : Promise<boolean> {
+  const userLoginInformation = await prisma.user.findUnique({
+    where: {
+      user_email : userEmail,
+    },
+  });
+    
+  if (!userLoginInformation?.user_password_hash) {
+    return false;
+  }
+
+  const storedPassword = userLoginInformation.user_password_hash as string;
+  const isValidUser = await bcrypt.compare(userPassword, storedPassword);
+
+  return isValidUser;
+}
+
+
+export default {
+  validateUser,
+};
