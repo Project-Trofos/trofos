5bb100decc28e276339bf91bb54ea86a2710c299
KishenKumarrrrr <kishen1198@gmail.com>
2022-08-30 23:54:37 +0800

    feat(backend): add session service

--- packages/backend/src/services/session.service.ts
+++ packages/backend/src/services/session.service.ts
@@ -26,6 +26,8 @@ async function createUserSession(userEmail: string, prisma: PrismaClient) : Prom
       if (prismaError.code !== PRISMA_UNIQUE_CONSTRAINT_VIOLATION_ERROR_CODE) {
         throw e;
       }
+      // If the error was due to a unique constraint violation, we generate the sessionId again.
+      sessionId = crypto.randomBytes(16).toString('base64');
     }
   } while (!isSessionCreated);
 
