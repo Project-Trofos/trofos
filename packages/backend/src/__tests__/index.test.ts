0013b8dc160a6d57e4108dcfa2038e8ab18c6010
KishenKumarrrrr <kishen1198@gmail.com>
2022-08-31 10:15:42 +0800

    feat(backend): add /login route to server

--- packages/backend/src/__tests__/index.test.ts
+++ packages/backend/src/__tests__/index.test.ts
@@ -1,6 +1,13 @@
 import request from 'supertest';
 import server from '../index';
 
+import authentication from '../controllers/authentication';
+import sessionService from '../services/session.service';
+import authenticationService from '../services/authentication.service';
+
+const authenticationServiceValidateUserSpy = jest.spyOn(authenticationService, 'validateUser');
+const sessionServiceCreateUserSessionSpy = jest.spyOn(sessionService, 'createUserSession');
+
 afterAll(done => {
   server.close();
   done();
@@ -10,5 +17,19 @@ describe('index.ts', () => {
   test('/ route should return Hello World', async () => {
     const res = await request(server).get('/');
     expect(res.text).toEqual('Hello World!');
+  });
+
+  test('/login route should return session cookie', async () => {
+    const sessionId = 'testSession';
+    authenticationServiceValidateUserSpy.mockResolvedValueOnce(true);
+    sessionServiceCreateUserSessionSpy.mockResolvedValueOnce(sessionId);
+    const postData = {
+      body : {
+        userEmail : "testUser@test.com",
+        userPassword : "testPassword",
+      },
+    }
+    const res = await request(server).post('/login').send(postData);
+    expect(res.headers['set-cookie']).toContain("trofos_sessioncookie=testSession; Path=/");
   });
 });
