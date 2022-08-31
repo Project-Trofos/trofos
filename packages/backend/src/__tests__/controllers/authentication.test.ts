1cb20de78d7194c041f3111baa4638adda5dd7dd
KishenKumarrrrr <kishen1198@gmail.com>
2022-08-31 10:02:33 +0800

    feat(backend): add authentication controller

---
+++ packages/backend/src/__tests__/controllers/authentication.test.ts
@@ -0,0 +1,101 @@
+import express from 'express';
+import StatusCodes from 'http-status-codes';
+import { mockDeep } from 'jest-mock-extended';
+import { PrismaClient } from '.prisma/client';
+import authentication from '../../controllers/authentication';
+import sessionService from '../../services/session.service';
+import authenticationService from '../../services/authentication.service';
+
+
+
+const authenticationServiceValidateUserSpy = jest.spyOn(authenticationService, 'validateUser');
+const sessionServiceCreateUserSessionSpy = jest.spyOn(sessionService, 'createUserSession');
+const prismaMock = mockDeep<PrismaClient>();
+
+describe('authentication.loginUser tests', () => {
+  test('UserNotValid_HTTP401Returned', async () => {
+    authenticationServiceValidateUserSpy.mockResolvedValueOnce(false);
+    const testUserEmail = 'testEmail@test.com';
+    const testUserPassword = 'testPassword';
+    const mockRequest = {
+      body : {
+        userEmail : testUserEmail,
+        userPassword : testUserPassword,
+      },
+    } as express.Request;
+    const mockResponse = {
+      send() {},
+      status(s : number) {this.statusCode = s; return this;},
+    } as express.Response;
+    await authentication.loginUser(mockRequest, mockResponse, prismaMock);
+    expect(authenticationServiceValidateUserSpy).toHaveBeenCalledWith(testUserEmail, testUserPassword, prismaMock);
+    expect(sessionServiceCreateUserSessionSpy).toHaveBeenCalledTimes(0);
+    expect(mockResponse.statusCode).toEqual(StatusCodes.UNAUTHORIZED);
+  });
+
+  test('UserNotValid_ValidateUserThrowsError_HTTP500Returned', async () => {
+    const validateUserError = new Error('test error');
+    authenticationServiceValidateUserSpy.mockRejectedValueOnce(validateUserError);
+    const testUserEmail = 'testEmail@test.com';
+    const testUserPassword = 'testPassword';
+    const mockRequest = {
+      body : {
+        userEmail : testUserEmail,
+        userPassword : testUserPassword,
+      },
+    } as express.Request;
+    const mockResponse = {
+      send() {},
+      status(s : number) {this.statusCode = s; return this;},
+    } as express.Response;
+    await authentication.loginUser(mockRequest, mockResponse, prismaMock);
+    expect(authenticationServiceValidateUserSpy).toHaveBeenCalledWith(testUserEmail, testUserPassword, prismaMock);
+    expect(sessionServiceCreateUserSessionSpy).toHaveBeenCalledTimes(0);
+    expect(mockResponse.statusCode).toEqual(StatusCodes.INTERNAL_SERVER_ERROR);
+  });
+
+  test('UserValid_CreateUserSessionThrowsError_HTTP500Returned', async () => {
+    const createUserSessionError = new Error('test error');
+    authenticationServiceValidateUserSpy.mockResolvedValueOnce(true);
+    sessionServiceCreateUserSessionSpy.mockRejectedValueOnce(createUserSessionError);
+    const testUserEmail = 'testEmail@test.com';
+    const testUserPassword = 'testPassword';
+    const mockRequest = {
+      body : {
+        userEmail : testUserEmail,
+        userPassword : testUserPassword,
+      },
+    } as express.Request;
+    const mockResponse = {
+      send() {},
+      status(s : number) {this.statusCode = s; return this;},
+    } as express.Response;
+    await authentication.loginUser(mockRequest, mockResponse, prismaMock);
+    expect(authenticationServiceValidateUserSpy).toHaveBeenCalledWith(testUserEmail, testUserPassword, prismaMock);
+    expect(sessionServiceCreateUserSessionSpy).toHaveBeenCalledWith(testUserEmail, prismaMock);
+    expect(mockResponse.statusCode).toEqual(StatusCodes.INTERNAL_SERVER_ERROR);
+  });
+
+  test('UserValid_SessionReturned', async () => {
+    const sessionId = 'testSession';
+    authenticationServiceValidateUserSpy.mockResolvedValueOnce(true);
+    sessionServiceCreateUserSessionSpy.mockResolvedValueOnce(sessionId);
+    const testUserEmail = 'testEmail@test.com';
+    const testUserPassword = 'testPassword';
+    const mockRequest = {
+      body : {
+        userEmail : testUserEmail,
+        userPassword : testUserPassword,
+      },
+    } as express.Request;
+    const mockResponse = {
+      send() {},
+      status(s : number) {this.statusCode = s; return this;},
+      cookie(_name : string, _val : string, _opts : any) {},
+    } as express.Response;
+    await authentication.loginUser(mockRequest, mockResponse, prismaMock);
+    expect(authenticationServiceValidateUserSpy).toHaveBeenCalledWith(testUserEmail, testUserPassword, prismaMock);
+    expect(sessionServiceCreateUserSessionSpy).toHaveBeenCalledWith(testUserEmail, prismaMock);
+    expect(mockResponse.statusCode).toEqual(StatusCodes.OK);
+  });
+});
---
+++ packages/backend/src/controllers/authentication.ts
@@ -0,0 +1,32 @@
+import { PrismaClient } from '.prisma/client';
+import StatusCodes from 'http-status-codes';
+import express from 'express';
+import authenticationService  from '../services/authentication.service';
+import sessionService from '../services/session.service';
+
+const TROFOS_SESSIONCOOKIE = 'trofos_sessioncookie';
+
+const loginUser = async (req : express.Request, res: express.Response, prisma : PrismaClient) => {
+  const { userEmail, userPassword } = req.body;
+
+  try {
+    const isValidUser = await authenticationService.validateUser(userEmail, userPassword, prisma);
+
+    if (!isValidUser) {
+      return res.status(StatusCodes.UNAUTHORIZED).send();
+    }
+  
+    const sessionId = await sessionService.createUserSession(userEmail, prisma);
+
+    res.cookie(TROFOS_SESSIONCOOKIE, sessionId);
+    res.status(StatusCodes.OK).send();
+  } catch (e) {
+    console.error(e);
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send();
+  }
+
+};
+
+export default {
+  loginUser,
+};
