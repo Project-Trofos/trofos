9346bc62342b56466cc2602795bebca60f90e262
luo-git <lluoyi.lu@gmail.com>
2022-09-01 23:57:09 +0800

    fix(frontend): fix form error

--- packages/frontend/src/components/modals/ProjectCreationModal.tsx
+++ packages/frontend/src/components/modals/ProjectCreationModal.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Button, Form, Input, Modal } from 'antd';
 import { InfoCircleOutlined } from '@ant-design/icons';
 import { useAddProjectMutation } from '../../api';
@@ -12,10 +12,6 @@ export default function ProjectCreationModal() {
   const [addProject] = useAddProjectMutation();
 
   const [form] = Form.useForm();
-
-  useEffect(() => {
-    form.validateFields(['projectName']);
-  }, [form]);
 
   const showModal = () => {
     setIsModalVisible(true);
@@ -46,6 +42,7 @@ export default function ProjectCreationModal() {
         visible={isModalVisible}
         onOk={handleOk}
         onCancel={handleCancel}
+        getContainer={false}
         footer={[
           <Button key="cancel" onClick={handleCancel}>
             Cancel
