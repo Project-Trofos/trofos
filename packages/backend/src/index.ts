2b0692b9e9133630fbc43cf98264fa479302250c
KishenKumarrrrr <kishen1198@gmail.com>
2022-09-01 22:26:02 +0800

    refactor(backend): remove body-parser

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,6 +1,5 @@
 import express from 'express';
 import { PrismaClient } from '@prisma/client';
-import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import authentication from './controllers/authentication';
 
@@ -8,7 +7,7 @@ const app = express();
 const port = 3001;
 const prisma = new PrismaClient();
 
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(cookieParser());
 
 app.get('/', (req : express.Request, res: express.Response) => {
