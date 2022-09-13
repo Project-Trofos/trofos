8591b7bcd559c080cc69e36e48b1ac45bfc34aef
Jonah Tan <47470981+jonahtanjz@users.noreply.github.com>
2022-09-13 22:37:48 +0800

    test(frontend): add unit tests for backlog listing UI components

---
+++ packages/frontend/src/components/cards/BacklogCard.test.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import BacklogCard from './BacklogCard';
+import type { Backlog } from '../../api/backlog';
+
+describe('BacklogCard test', () => {
+  const mockBacklog: Backlog = {
+    id: 111,
+    summary: 'A Test Summary',
+    type: 'story',
+    priority: 'very_high',
+    sprint_id: 123,
+    reporter_id: 1,
+    assignee_id: 222,
+    points: 3,
+    description: 'A test description here',
+    project_id: 123,
+  };
+  const { baseElement } = render(<BacklogCard backlog={mockBacklog} />);
+	
+  it('renders backlog card with correct details', () => {
+    expect(screen.getByText('111')).toBeInTheDocument();
+    expect(screen.getByText('A Test Summary')).toBeInTheDocument();
+    expect(screen.getByText('story')).toBeInTheDocument();
+    expect(screen.getByText('very_high')).toBeInTheDocument();
+    expect(screen.getByText('222')).toBeInTheDocument();
+    expect(screen.getByText('3')).toBeInTheDocument();
+  
+    // Compare with snapshot to ensure structure remains the same
+    expect(baseElement).toMatchSnapshot();
+  });
+});
--- packages/frontend/src/components/modals/BacklogModal.test.tsx
+++ packages/frontend/src/components/modals/BacklogModal.test.tsx
@@ -2,24 +2,26 @@ import React from 'react';
 import { fireEvent, render, screen } from '@testing-library/react';
 import BacklogModal from './BacklogModal';
 
-test('renders new backlog modal with correct fields', () => {
-  const mockUpdateBacklogs = jest.fn();
-  const { baseElement } = render(<BacklogModal updateBacklogs={mockUpdateBacklogs} />);
-
-  // Open modal
-  const button = screen.getByText(/New Backlog/i);
-  fireEvent.click(button);
-
-  // Ensure fields are present
-  expect(screen.getByPlaceholderText('* Type summary here...')).toBeInTheDocument();
-  expect(screen.getByLabelText('Type')).toBeInTheDocument();
-  expect(screen.getByLabelText('Sprint')).toBeInTheDocument();
-  expect(screen.getByLabelText('Priority')).toBeInTheDocument();
-  expect(screen.getByLabelText('Reporter')).toBeInTheDocument();
-  expect(screen.getByLabelText('Assignee')).toBeInTheDocument();
-  expect(screen.getByLabelText('Points')).toBeInTheDocument();
-  expect(screen.getByPlaceholderText('Description...')).toBeInTheDocument();
-
-  // Compare with snapshot to ensure structure remains the same
-  expect(baseElement).toMatchSnapshot();
+describe('BacklogModal tests', () => {
+  test('renders new backlog modal with correct fields', () => {
+    const mockUpdateBacklogs = jest.fn();
+    const { baseElement } = render(<BacklogModal updateBacklogs={mockUpdateBacklogs} />);
+  
+    // Open modal
+    const button = screen.getByText(/New Backlog/i);
+    fireEvent.click(button);
+  
+    // Ensure fields are present
+    expect(screen.getByPlaceholderText('* Type summary here...')).toBeInTheDocument();
+    expect(screen.getByLabelText('Type')).toBeInTheDocument();
+    expect(screen.getByLabelText('Sprint')).toBeInTheDocument();
+    expect(screen.getByLabelText('Priority')).toBeInTheDocument();
+    expect(screen.getByLabelText('Reporter')).toBeInTheDocument();
+    expect(screen.getByLabelText('Assignee')).toBeInTheDocument();
+    expect(screen.getByLabelText('Points')).toBeInTheDocument();
+    expect(screen.getByPlaceholderText('Description...')).toBeInTheDocument();
+  
+    // Compare with snapshot to ensure structure remains the same
+    expect(baseElement).toMatchSnapshot();
+  });
 });
