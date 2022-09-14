dfedae34e3e2bc30d08f3441b83964cf696a9b8e
Jonah Tan <47470981+jonahtanjz@users.noreply.github.com>
2022-09-15 02:13:30 +0800

    feat(backend): add seed script for backlog feature

--- packages/backend/prisma/seed.ts
+++ packages/backend/prisma/seed.ts
@@ -1,17 +1,19 @@
-import { PrismaClient } from "@prisma/client"
-import { createUserSeed } from "./seeds/authentication.seed"
-const prisma = new PrismaClient()
+import { PrismaClient } from '@prisma/client';
+import { createUserSeed } from './seeds/authentication.seed';
+import { setupBacklogSeed } from './seeds/backlog.seed';
+const prisma = new PrismaClient();
 
 async function main() {
-    createUserSeed(prisma)
+  createUserSeed(prisma);
+  setupBacklogSeed(prisma);
 }
 
 main()
-    .then(async () => {
-        await prisma.$disconnect()
-    })
-    .catch(async (e) => {
-        console.error(e)
-        await prisma.$disconnect()
-        process.exit(1)
-    })
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
