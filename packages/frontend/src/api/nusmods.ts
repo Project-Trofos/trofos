ae3aed695db3a9ae33a5ece227fcee13b723cdaa
luo-git <lluoyi.lu@gmail.com>
2022-09-14 14:30:10 +0800

    feat(frontend): add NUSMODS api slice

---
+++ packages/frontend/src/api/nusmods.ts
@@ -0,0 +1,19 @@
+import { nusmodsApiSlice } from '.';
+
+export type Module = {
+  moduleCode: string;
+  title: string;
+  semesters: number[],
+};
+
+// Project management APIs
+const extendedApi = nusmodsApiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getAllModules: builder.query<Module[], void>({
+      query: () => '/moduleList.json',
+    }),
+  }),
+  overrideExisting: false,
+});
+
+export const { useGetAllModulesQuery } = extendedApi;
