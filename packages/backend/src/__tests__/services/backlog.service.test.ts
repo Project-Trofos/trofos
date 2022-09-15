5398ef686be00507311443569f7190451dc5d480
Jonah Tan <47470981+jonahtanjz@users.noreply.github.com>
2022-09-15 15:03:49 +0800

    feat: add backlog listing feature (#12)
    * feat: add backlog listing
    
    * feat(frontend): add UI for listing backlogs
    
    * test(backend): add unit tests to backlog listing controller and service
    
    * test(frontend): add unit tests for backlog listing UI components
    
    * style(frontend): fix code styling on frontend components
    
    * feat(frontend): add styling to backlog listing card
    
    * feat: change fetch to RTK query

--- packages/backend/src/__tests__/services/backlog.service.test.ts
+++ packages/backend/src/__tests__/services/backlog.service.test.ts
@@ -3,62 +3,98 @@ import { prismaMock } from '../../models/mock/mockPrismaClient';
 import backlogService from '../../services/backlog.service';
 import { BacklogFields } from '../../services/types/backlog.service.types';
 
-describe('backlog.service tests', () => {
-  test('createBacklog_ValidBacklog_ReturnsBacklog', async () => {
-    const mockReturnedBacklog: Backlog = {
-      id: 1,
-      summary: 'A Test Summary',
-      type: 'story',
-      priority: 'very_high',
-      sprint_id: 123,
-      reporter_id: 1,
-      assignee_id: 1,
-      points: 1,
-      description: 'A test description here',
-      project_id: 123,
-    };
-
-    const backlog: BacklogFields = {
-      assigneeId: 1,
-      description: 'A test description here',
-      points: 1,
-      priority: 'very_high',
-      projectId: 123,
-      reporterId: 1,
-      summary: 'A Test Summary',
-      sprintId: 123,
-      type: 'story',
-    };
-    prismaMock.backlog.create.mockResolvedValue(mockReturnedBacklog);
-    await expect(backlogService.createBacklog(backlog)).resolves.toEqual(mockReturnedBacklog);
+describe('backlog.service tests',  ()=> {
+  describe('create backlog', () => {
+    it('should create and return backlog when called with valid fields', async () => {
+      const mockReturnedBacklog: Backlog = {
+        id: 1,
+        summary: 'A Test Summary',
+        type: 'story',
+        priority: 'very_high',
+        sprint_id: 123,
+        reporter_id: 1,
+        assignee_id: 1,
+        points: 1,
+        description: 'A test description here',
+        project_id: 123,
+      };
+  
+      const backlog: BacklogFields = {
+        assigneeId: 1,
+        description: 'A test description here',
+        points: 1,
+        priority: 'very_high',
+        projectId: 123,
+        reporterId: 1,
+        summary: 'A Test Summary',
+        sprintId: 123,
+        type: 'story',
+      };
+      prismaMock.backlog.create.mockResolvedValue(mockReturnedBacklog);
+      await expect(backlogService.createBacklog(backlog)).resolves.toEqual(mockReturnedBacklog);
+    });
+  
+    it('should create and return backlog when optional fields are omitted', async () => {
+      const mockReturnedBacklog: Backlog = {
+        id: 1,
+        summary: 'A Test Summary',
+        type: 'story',
+        priority: null,
+        sprint_id: null,
+        reporter_id: 1,
+        assignee_id: null,
+        points: null,
+        description: null,
+        project_id: 123,
+      };
+  
+      const backlog: BacklogFields = {
+        assigneeId: undefined,
+        description: undefined,
+        points: undefined,
+        priority: undefined,
+        projectId: 123,
+        reporterId: 1,
+        summary: 'A Test Summary',
+        sprintId: undefined,
+        type: 'story',
+      };
+      prismaMock.backlog.create.mockResolvedValueOnce(mockReturnedBacklog);
+      await expect(backlogService.createBacklog(backlog)).resolves.toEqual(mockReturnedBacklog);
+    });
   });
-
-  test('createBacklog_OmitOptionalFields_ReturnsBacklog', async () => {
-    const mockReturnedBacklog: Backlog = {
-      id: 1,
-      summary: 'A Test Summary',
-      type: 'story',
-      priority: null,
-      sprint_id: null,
-      reporter_id: 1,
-      assignee_id: null,
-      points: null,
-      description: null,
-      project_id: 123,
-    };
-
-    const backlog: BacklogFields = {
-      assigneeId: undefined,
-      description: undefined,
-      points: undefined,
-      priority: undefined,
-      projectId: 123,
-      reporterId: 1,
-      summary: 'A Test Summary',
-      sprintId: undefined,
-      type: 'story',
-    };
-    prismaMock.backlog.create.mockResolvedValueOnce(mockReturnedBacklog);
-    await expect(backlogService.createBacklog(backlog)).resolves.toEqual(mockReturnedBacklog);
+  
+  describe('get backlogs', () => {
+    it('should return backlogs when called with valid project id', async () => {
+      const mockReturnedBacklogs: Backlog[] = [
+        {
+          id: 1,
+          summary: 'A Test Summary',
+          type: 'story',
+          priority: 'very_high',
+          sprint_id: 123,
+          reporter_id: 1,
+          assignee_id: 1,
+          points: 1,
+          description: 'A test description here',
+          project_id: 123,
+        },
+        {
+          id: 2,
+          summary: 'Another Test Summary',
+          type: 'task',
+          priority: 'high',
+          sprint_id: 123,
+          reporter_id: 1,
+          assignee_id: 2,
+          points: 1,
+          description: 'Another test description here',
+          project_id: 123,
+        },
+      ];
+      const projectId = 123;
+      prismaMock.backlog.findMany.mockResolvedValueOnce(mockReturnedBacklogs);
+      await expect(backlogService.getBacklogs(projectId)).resolves.toEqual(mockReturnedBacklogs);
+    });
   });
 });
