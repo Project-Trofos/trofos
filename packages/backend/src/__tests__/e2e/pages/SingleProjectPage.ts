8fc7d234c960ec088f98301a73f5c736aae4e2d0
KishenKumarrrrr <kishen1198@gmail.com>
2022-09-24 18:56:01 +0800

    test(e2e): update e2e tests to make them more robust

---
+++ packages/backend/src/__tests__/e2e/pages/SingleProjectPage.ts
@@ -0,0 +1,78 @@
+import { WebDriver, By } from 'selenium-webdriver';
+import ProjectsPage from './ProjectsPage';
+
+export default class SingleProjectPage {
+  readonly driver : WebDriver;
+
+  constructor(driver : WebDriver) {
+    this.driver = driver;
+  }
+
+  async deleteProject() : Promise<ProjectsPage> {
+    const dropdownMenu = await this.driver.findElement(By.css('button.ant-dropdown-trigger'));
+    const actions = this.driver.actions({ async : true });
+    await actions.move({ origin: dropdownMenu }).press().perform();
+
+    this.driver.switchTo().activeElement();
+    await this.driver.findElement(By.xpath("//span[text()='Delete project']")).click();
+    this.driver.switchTo().activeElement();
+    await this.driver.findElement(By.xpath("//span[text()='OK']")).click();
+
+    return new ProjectsPage(this.driver);
+  }
+
+  async clickOnBacklogs() {
+    this.driver.findElement(By.xpath("//a[text()='Backlog']")).click();
+  }
+
+  async getNumberOfBacklogItems() : Promise<number> {
+    const numberOfBacklogItems = (await this.driver.findElements(By.css('ul > *.backlog-card-container'))).length;
+    return numberOfBacklogItems;
+  }
+
+  async clickOnCreateBacklogButton() {
+    await this.driver.findElement(By.xpath("//span[text()='New Backlog']")).click();
+    await this.driver.switchTo().activeElement();
+  }
+
+  async addBacklogSummary(summary : string) {
+    await this.driver.findElement(By.css('input#summary')).sendKeys(summary);
+  }
+
+  async selectBacklogType(type : string) {
+    await this.driver.findElement(By.xpath("//input[@id='type']")).click();
+    await this.driver.findElement(By.xpath(`//div[text()='${type}']`)).click();
+  }
+
+  async selectBacklogSprintId(sprintId : string) {
+    await this.driver.findElement(By.xpath("//input[@id='sprintId']")).click();
+    await this.driver.findElement(By.xpath(`//div[text()='${sprintId}']`)).click();
+  }
+
+  async selectBacklogPriority(priority : string) {
+    await this.driver.findElement(By.xpath("//input[@id='priority']")).click();
+    await this.driver.findElement(By.xpath(`//div[text()='${priority}']`)).click();
+  }
+
+  async selectBacklogReporter(reporterId : string) {
+    await this.driver.findElement(By.xpath("//input[@id='reporterId']")).click();
+    await this.driver.findElement(By.xpath(`//div[@id='reporterId_list']/following-sibling::div//span[text()='${reporterId}']`)).click();
+  }
+
+  async selectBacklogAssignee(assigneeId : string) {
+    await this.driver.findElement(By.xpath("//input[@id='assigneeId']")).click();
+    await this.driver.findElement(By.xpath(`//div[@id='assigneeId_list']/following-sibling::div//span[text()='${assigneeId}']`)).click();
+  }
+
+  async assignBacklogPoints(points : number) {
+    await this.driver.findElement(By.xpath("//input[@id='points']")).sendKeys(points);
+  }
+
+  async addBacklogDescription(description : string) {
+    await this.driver.findElement(By.xpath("//textarea[@id='description']")).sendKeys(description);
+  }
+
+  async clickCreateInBacklogForm() {
+    await this.driver.findElement(By.xpath("//span[text()='Create']")).click();
+  }
+}
---
+++ packages/backend/src/__tests__/e2e/tests/project.test.ts
@@ -0,0 +1,86 @@
+import { Browser, Builder, WebDriver } from 'selenium-webdriver';
+import chrome from 'selenium-webdriver/chrome';
+import HomePage from '../pages/HomePage';
+
+jest.setTimeout(30000);
+
+let driver: WebDriver;
+
+beforeEach(async () => {
+  if (process.env.CI) {
+    driver = await new Builder().forBrowser(Browser.CHROME)
+      .setChromeOptions(new chrome.Options()
+        .addArguments('--no-sandbox', '--headless')).build();
+  } else {
+    driver = await new Builder().forBrowser(Browser.CHROME).build();
+    driver.manage().setTimeouts({ implicit : 10000 });
+  }
+});
+
+afterEach(async () => {
+  await driver.quit();
+});
+
+describe('Projects', () => {
+
+  describe('when the user tries to create/delete a project', () => {
+
+    it('should successfully create/delete the project', async () => {
+
+      const homePage = new HomePage(driver);
+      homePage.getPage();
+
+      const loginPage = await homePage.clickLoginButton();
+      let projectsPage = await loginPage.loginWithCredentials('testUser@test.com', 'testPassword');
+
+      const numberOfProjectsAtStart = await projectsPage.getNumberOfProjects();
+      await projectsPage.createNewProjectWithFields('testProject', 'TEST');
+      const numberOfProjectsAfterCreation = await projectsPage.getNumberOfProjects();
+
+      await expect(numberOfProjectsAtStart + 1 === numberOfProjectsAfterCreation);
+
+      const singleProjectPage = await projectsPage.clickOnProjectWithName('testProject');
+      projectsPage = await singleProjectPage.deleteProject();
+
+      const numberOfProjectsAfterDeletion = await projectsPage.getNumberOfProjects();
+
+      await expect(numberOfProjectsAtStart === numberOfProjectsAfterDeletion);
+
+      await projectsPage.clickLogoutButton();
+    });
+
+  });
+
+  describe('when the user tries to create a backlog', () => {
+
+    it('should successfully create the backlog', async () => {
+
+      const homePage = new HomePage(driver);
+      homePage.getPage();
+
+      const loginPage = await homePage.clickLoginButton();
+      const projectsPage = await loginPage.loginWithCredentials('testUser@test.com', 'testPassword');
+
+      const singleProjectPage = await projectsPage.clickOnProjectWithName('Backlog test project');
+      await singleProjectPage.clickOnBacklogs();
+
+      const numberOfBacklogItemsAtStart = await singleProjectPage.getNumberOfBacklogItems();
+
+      await singleProjectPage.clickOnCreateBacklogButton();
+      await singleProjectPage.addBacklogSummary('Test Backlog');
+      await singleProjectPage.selectBacklogType('Task');
+      await singleProjectPage.selectBacklogSprintId('Sprint 1');
+      await singleProjectPage.selectBacklogPriority('High');
+      await singleProjectPage.selectBacklogReporter('User1');
+      await singleProjectPage.selectBacklogAssignee('User1');
+      await singleProjectPage.assignBacklogPoints(4);
+      await singleProjectPage.addBacklogDescription('Test Description');
+      await singleProjectPage.clickCreateInBacklogForm();
+
+      const numberOfBacklogItemsAtEnd = await singleProjectPage.getNumberOfBacklogItems();
+      
+      expect(numberOfBacklogItemsAtStart === numberOfBacklogItemsAtEnd + 1);
+    });
+
+  });
+});
