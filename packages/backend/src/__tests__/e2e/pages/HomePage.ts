1afe31ac76f67ac89a60c1f9ea1ab7c5d9cf8b5f
Kishen Kumar <kishen1198@gmail.com>
2022-09-25 09:19:48 +0800

    test(e2e): update e2e tests to make them more robust (#18)
    * test(e2e): update e2e tests to make them more robust
    
    * test(e2e): update selenium driver to implicitly wait regardless of environment

---
+++ packages/backend/src/__tests__/e2e/pages/HomePage.ts
@@ -0,0 +1,23 @@
+import { By, WebDriver } from 'selenium-webdriver';
+import LoginPage from './LoginPage';
+
+export default class HomePage {
+  readonly driver : WebDriver;
+
+  constructor(driver : WebDriver) {
+    this.driver = driver;
+  }
+
+  async getPage() {
+    await this.driver.get('http://localhost:3000');
+  }
+
+  async clickLoginButton() : Promise<LoginPage> {
+    await this.driver.findElement(By.css('a[href=\'/login\']')).click();
+    return new LoginPage(this.driver);
+  }
+
+
+
+
+}
---
+++ packages/backend/src/__tests__/e2e/pages/LoginPage.ts
@@ -0,0 +1,17 @@
+import { WebDriver, By } from 'selenium-webdriver';
+import ProjectsPage from './ProjectsPage';
+
+export default class LoginPage {
+  readonly driver : WebDriver;
+
+  constructor(driver : WebDriver) {
+    this.driver = driver;
+  }
+
+  async loginWithCredentials(email : string, password : string) : Promise<ProjectsPage> {
+    await this.driver.findElement(By.id('basic_userEmail')).sendKeys(email);
+    await this.driver.findElement(By.id('basic_userPassword')).sendKeys(password);
+    await this.driver.findElement(By.xpath("//span[text()='Submit']")).click();
+    return new ProjectsPage(this.driver);
+  }
+}
