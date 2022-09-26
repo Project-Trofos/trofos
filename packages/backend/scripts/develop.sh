4dacb1b59e04d8d4cdd591da3bee66457b8a7497
KishenKumarrrrr <kishen1198@gmail.com>
2022-09-26 12:07:43 +0800

    chore: modify development db set up script

--- packages/backend/scripts/develop.sh
+++ packages/backend/scripts/develop.sh
@@ -4,6 +4,8 @@
 npx dotenv -e .env.development.local -- npx prisma generate
 
 # reset the database
-yes | npx dotenv -e .env.development.local -- npx prisma migrate reset
+yes | npx dotenv -e .env.development.local -- npx prisma migrate reset --skip-seed
+npx dotenv -e .env.development.local -- npx prisma db push
+npx dotenv -e .env.development.local -- npx prisma db seed
 
 echo "Development set up completed."
