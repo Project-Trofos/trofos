760de21e34b0855f75220b8fe1bbe77003801329
luo-git <lluoyi.lu@gmail.com>
2022-09-27 17:03:27 +0800

    feat(frontend): update projects table

--- packages/frontend/src/components/tables/ProjectTable.tsx
+++ packages/frontend/src/components/tables/ProjectTable.tsx
@@ -6,65 +6,75 @@ import { confirmDeleteProject, confirmDetachProject } from '../modals/confirm';
 import { useRemoveProjectFromCourseMutation } from '../../api/course';
 import { getErrorMessage } from '../../helpers/error';
 
-
-
 /**
  * Table for listing projects
  */
-export default function ProjectTable({ projects, isLoading }: { projects: Project[], isLoading: boolean }) {
-
+export default function ProjectTable({ projects, isLoading }: { projects: Project[]; isLoading: boolean }) {
   const [removeProject] = useRemoveProjectMutation();
   const [removeProjectFromCourse] = useRemoveProjectFromCourseMutation();
 
-  const handleDeleteProject = useCallback((project: Project) => {
-    try {
-      confirmDeleteProject(async () => {
-        await removeProject({ id: project.id }).unwrap();
-      });
-    } catch (err) {
-      message.error(getErrorMessage(err));
-    }
-  }, [removeProject]);
-
-  const handleRemoveProjectFromCourse = useCallback(async (project: Project) => {
-    try {
-      if (!project.course_id) {
-        throw Error('Invalid project to be deleted!');
+  const handleDeleteProject = useCallback(
+    (project: Project) => {
+      try {
+        confirmDeleteProject(async () => {
+          await removeProject({ id: project.id }).unwrap();
+        });
+      } catch (err) {
+        message.error(getErrorMessage(err));
       }
+    },
+    [removeProject],
+  );
 
-      confirmDetachProject(async () => {
-        removeProjectFromCourse({ projectId: project.id, courseId: project.course_id ?? '' }).unwrap();
-      });
-    } catch (err) {
-      message.error(getErrorMessage(err));
-    }
-  }, [removeProjectFromCourse]);
+  const handleRemoveProjectFromCourse = useCallback(
+    async (project: Project) => {
+      try {
+        if (!project.course_id || !project.course_year || !project.course_sem) {
+          throw Error('Invalid project to be deleted!');
+        }
+
+        confirmDetachProject(async () => {
+          removeProjectFromCourse({
+            projectId: project.id,
+            courseId: project.course_id as string,
+            courseYear: project.course_year as number,
+            courseSem: project.course_sem as number,
+          }).unwrap();
+        });
+      } catch (err) {
+        message.error(getErrorMessage(err));
+      }
+    },
+    [removeProjectFromCourse],
+  );
 
   return (
     <Card>
-      <Space direction='vertical' size='middle' style={{ display: 'flex' }}>
+      <Space direction="vertical" size="middle" style={{ display: 'flex' }}>
         <Typography style={{ fontSize: '2em' }}>Projects</Typography>
         <Table
           dataSource={projects}
           rowKey={(project) => project.id}
-          loading={isLoading} 
+          loading={isLoading}
           bordered
           pagination={{ pageSize: 5 }}
         >
           <Table.Column title="ID" dataIndex="id" />
           <Table.Column title="Name" dataIndex="pname" />
-          <Table.Column 
-            title="Action" 
-            dataIndex="action" 
+          <Table.Column
+            title="Action"
+            dataIndex="action"
             render={(_, record: Project) => (
               <Space size="middle">
                 <Link to={`/project/${record.id}`}>Go to</Link>
-                <Button size='small' onClick={() => handleDeleteProject(record)}>Delete</Button>
-                {record.course_id &&
-                  <Button size='small' onClick={() => handleRemoveProjectFromCourse(record)}>
+                <Button size="small" onClick={() => handleDeleteProject(record)}>
+                  Delete
+                </Button>
+                {record.course_id && (
+                  <Button size="small" onClick={() => handleRemoveProjectFromCourse(record)}>
                     Detach
                   </Button>
-                }
+                )}
               </Space>
             )}
           />
