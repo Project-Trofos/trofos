7b7161335768b22c623e1876f4809949015c71f5
Jonah Tan <47470981+jonahtanjz@users.noreply.github.com>
2022-12-09 00:56:58 +0800

    fix: add check on backlog points for decimal values

--- packages/frontend/src/components/fields/BacklogCardPoints.tsx
+++ packages/frontend/src/components/fields/BacklogCardPoints.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { InputNumber, message } from 'antd';
 import { useParams } from 'react-router-dom';
 import { useUpdateBacklogMutation } from '../../api/backlog';
@@ -10,11 +10,18 @@ export default function BacklogCardPoints(props: { backlogId: number; currentPoi
   const params = useParams();
   const projectId = Number(params.projectId);
   const [updateBacklog] = useUpdateBacklogMutation();
+  const [isError, setIsError] = useState(false);
 
   const handlePointsChange = async (event: React.FocusEvent<HTMLInputElement, Element>) => {
     const updatedPoints = Number(event.target.value);
+    if (!Number.isInteger(updatedPoints)) {
+      setIsError(true);
+      message.error({ content: 'Points must be an integer', key: 'backlogUpdateMessage' });
+      return;
+    }
 
     if (updatedPoints === currentPoints) {
+      setIsError(false);
       return;
     }
 
@@ -28,6 +35,7 @@ export default function BacklogCardPoints(props: { backlogId: number; currentPoi
 
     try {
       await updateBacklog(payload).unwrap();
+      setIsError(false);
       message.success({ content: 'Backlog updated', key: 'backlogUpdateMessage' });
     } catch (e) {
       message.error({ content: 'Failed to update backlog', key: 'backlogUpdateMessage' });
@@ -43,6 +51,7 @@ export default function BacklogCardPoints(props: { backlogId: number; currentPoi
       onBlur={handlePointsChange}
       min={1}
       controls={false}
+      status={isError ? 'error' : undefined}
     />
   );
 }
