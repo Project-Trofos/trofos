5407c7c58e7a8230ea77ecfa338ec393641540dd
luo-git <lluoyi.lu@gmail.com>
2023-01-02 19:02:38 +0800

    test(backend): update mock data

--- packages/backend/src/__tests__/mocks/announcementData.ts
+++ packages/backend/src/__tests__/mocks/announcementData.ts
@@ -4,6 +4,7 @@ export const announcementData: Announcement[] = [
   {
     id: 1,
     course_id: 1,
+    user_id: 1,
     title: 'announcement 1',
     content: 'announcement 1 content',
     created_at: new Date(),
@@ -12,6 +13,7 @@ export const announcementData: Announcement[] = [
   {
     id: 2,
     course_id: 1,
+    user_id: 1,
     title: 'announcement 2',
     content: 'announcement 2 content',
     created_at: new Date(),
