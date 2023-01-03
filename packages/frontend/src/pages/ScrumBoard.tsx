366a3eb8154911fd0a243aa839332541253373c8
Jonah Tan <47470981+jonahtanjz@users.noreply.github.com>
2023-01-03 17:58:04 +0800

    feat(frontend): add backlog card for scrum board

--- packages/frontend/src/pages/ScrumBoard.tsx
+++ packages/frontend/src/pages/ScrumBoard.tsx
@@ -1,11 +1,37 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import { Backlog } from '../api/backlog';
+import { useGetBacklogStatusQuery, useGetProjectQuery } from '../api/project';
 import { useGetActiveSprintQuery } from '../api/sprint';
+import ScrumBoardCard from '../components/cards/ScrumBoardCard';
 
 export default function ScrumBoard(): JSX.Element {
   const params = useParams();
   const projectId = Number(params.projectId);
   const { data: activeSprint } = useGetActiveSprintQuery(projectId);
+  const { data: backlogStatus } = useGetBacklogStatusQuery({ id: projectId });
+  const { data: projectData } = useGetProjectQuery({ id: projectId });
 
-  return <h1 style={{ margin: 20 }}>Scrum Board Placeholder</h1>;
+  const processBacklogs = (backlogs?: Backlog[]) => {
+    if (!backlogs) {
+      return undefined;
+    }
+
+    return [...backlogs].sort((b1, b2) => b1.backlog_id - b2.backlog_id);
+  };
+
+  const backlogs = processBacklogs(activeSprint?.backlogs);
+
+  return (
+    <div>
+      {backlogs?.map((backlog) => (
+        <ScrumBoardCard
+          key={backlog.backlog_id}
+          backlog={backlog}
+          users={projectData?.users}
+          projectKey={projectData?.pkey}
+        />
+      ))}
+    </div>
+  );
 }
