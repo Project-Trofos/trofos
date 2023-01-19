878b956d7df2a73ab01903bd6f70ff7295b6a89d
KishenKumarrrrr <kishen1198@gmail.com>
2023-01-19 10:40:57 +0800

    build: update db seed

--- packages/backend/prisma/usersOnRolesOnCourses.seed.ts
+++ packages/backend/prisma/usersOnRolesOnCourses.seed.ts
@@ -1,9 +1,13 @@
 /* eslint-disable import/prefer-default-export */
 import { PrismaClient } from '@prisma/client';
 import { 
+    BACKLOG_USER_1_EMAIL,
+    BACKLOG_USER_2_EMAIL,
     COURSE_1_ID,
     COURSE_3_ID,
     FACULTY_ROLE_ID,
+    SHADOW_COURSE_1_ID,
+    SHADOW_COURSE_2_ID,
     STUDENT_ROLE_ID,
     USER_1_EMAIL,
     USER_2_EMAIL,
@@ -27,7 +31,22 @@ async function createUsersOnRolesOnCoursesTableSeed(prisma : PrismaClient) {
                 user_email : USER_1_EMAIL,
                 role_id : STUDENT_ROLE_ID,
                 course_id : COURSE_1_ID
-            }
+            },
+            {
+                user_email : BACKLOG_USER_1_EMAIL,
+                role_id : STUDENT_ROLE_ID,
+                course_id : SHADOW_COURSE_1_ID 
+            },
+            {
+                user_email : BACKLOG_USER_2_EMAIL,
+                role_id : STUDENT_ROLE_ID,
+                course_id : SHADOW_COURSE_1_ID 
+            },
+            {
+                user_email : USER_2_EMAIL,
+                role_id : FACULTY_ROLE_ID,
+                course_id : SHADOW_COURSE_2_ID
+            },
         ]
     });
 
