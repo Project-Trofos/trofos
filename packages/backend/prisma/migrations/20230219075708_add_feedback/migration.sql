91ae0d7d04a9c38bc4c40b6b9d69b0cbbb8747d7
luo-git <lluoyi.lu@gmail.com>
2023-02-19 15:57:54 +0800

    feat(backend): update migration

--- packages/backend/prisma/migrations/20230219075708_add_feedback/migration.sql
+++ packages/backend/prisma/migrations/20230219075708_add_feedback/migration.sql
@@ -1,3 +1,16 @@
+-- AlterEnum
+-- This migration adds more than one value to an enum.
+-- With PostgreSQL versions 11 and earlier, this is not possible
+-- in a single migration. This can be worked around by creating
+-- multiple migrations, each migration adding only one value to
+-- the enum.
+
+
+ALTER TYPE "Action" ADD VALUE 'create_feedback';
+ALTER TYPE "Action" ADD VALUE 'read_feedback';
+ALTER TYPE "Action" ADD VALUE 'update_feedback';
+ALTER TYPE "Action" ADD VALUE 'delete_feedback';
+
 -- AlterTable
 ALTER TABLE "UserSession" ALTER COLUMN "session_expiry" SET DEFAULT NOW() + interval '1 day';
 
@@ -12,9 +25,6 @@ CREATE TABLE "Feedback" (
 
     CONSTRAINT "Feedback_pkey" PRIMARY KEY ("id")
 );
-
--- CreateIndex
-CREATE UNIQUE INDEX "Feedback_sprint_id_key" ON "Feedback"("sprint_id");
 
 -- AddForeignKey
 ALTER TABLE "Feedback" ADD CONSTRAINT "Feedback_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("user_id") ON DELETE SET NULL ON UPDATE CASCADE;
