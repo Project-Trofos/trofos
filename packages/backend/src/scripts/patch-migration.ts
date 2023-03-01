d2b1988e62ddc3e3a1718fe7e44fc33b30d0e00e
KishenKumarrrrr <kishen1198@gmail.com>
2023-03-01 18:14:46 +0800

    style: fix style issues

--- packages/backend/src/scripts/patch-migration.ts
+++ packages/backend/src/scripts/patch-migration.ts
@@ -1,5 +1,4 @@
 import prisma from '../models/prismaClient';
-import { FACULTY_ROLE_ID, STUDENT_ROLE_ID } from '../helpers/constants';
 
 async function patchMigration() {
   await prisma.usersOnRolesOnCourses.createMany();
