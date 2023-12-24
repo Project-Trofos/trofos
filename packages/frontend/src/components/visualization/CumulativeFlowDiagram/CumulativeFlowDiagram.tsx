6d419c35abd242204decdb01e9ce321467fc1ec6
Chester Wong <psyforce99@gmail.com>
2023-12-24 15:39:18 +0800

    remove buggy slider and make x-axis proportional

--- packages/frontend/src/components/visualization/CumulativeFlowDiagram/CumulativeFlowDiagram.tsx
+++ packages/frontend/src/components/visualization/CumulativeFlowDiagram/CumulativeFlowDiagram.tsx
@@ -9,54 +9,16 @@ export function CumulativeFlowDiagram(props: { backlogHistory: BacklogHistory[] 
   const { backlogHistory } = props;
   const isDarkTheme = useAppSelector((state) => state.themeSlice.isDarkTheme);
   const data = useCummulativeFlow(backlogHistory);
-  // const [data, setData] = useState<Record<string, any>[]>([
-  //   {
-  //     type: 'Todo',
-  //     date: new Date(2023, 11, 24, 4, 10, 23),
-  //     value: 1,
-  //   },
-  //   {
-  //     type: 'Doing',
-  //     date: new Date(2023, 11, 24, 4, 10, 23),
-  //     value: 0,
-  //   },
-  //   {
-  //     type: 'Todo',
-  //     date: new Date(2023, 11, 25, 4, 10, 23),
-  //     value: 2,
-  //   },
-  //   {
-  //     type: 'Todo',
-  //     date: new Date(2023, 11, 27, 4, 10, 23),
-  //     value: 5,
-  //   },
-  //   {
-  //     type: 'Todo',
-  //     date: new Date(2023, 11, 28, 4, 10, 23),
-  //     value: 11,
-  //   },
-  //   {
-  //     type: 'Doing',
-  //     date: new Date(2023, 11, 29, 1, 10, 23),
-  //     value: 1,
-  //   },
-  //   {
-  //     type: 'Doing',
-  //     date: new Date(2023, 11, 29, 10, 10, 23),
-  //     value: 2,
-  //   },
-  // ]);
   const config: React.ComponentProps<typeof Area> = {
     data: data,
     theme: isDarkTheme ? 'dark' : 'default',
     xField: 'date',
     yField: 'value',
     seriesField: 'type',
-    slider: { formatter: dateFormatter },
     meta: {
       date: {
         alias: 'Date',
-        type: 'timeCat', // important to use this specific type.
+        type: 'time', // important to use this specific type.
         formatter: dateFormatter,
       },
       value: {
