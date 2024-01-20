5f60dc6c7a685995a85305173e60ae4f911447ee
Chester Wong <psyforce99@gmail.com>
2024-01-20 14:09:23 +0800

    Update CumulativeFlowCard.tsx

--- packages/frontend/src/components/cards/CumulativeFlowCard/CumulativeFlowCard.tsx
+++ packages/frontend/src/components/cards/CumulativeFlowCard/CumulativeFlowCard.tsx
@@ -9,12 +9,15 @@ import './CumulativeFlowCard.css';
 
 export default function CumulativeFlowCard(props: { backlogHistory: BacklogHistory[] }) {
   const { backlogHistory } = props;
-
+  const tooltipText = `Shows the statuses of issues over time. 
+    This helps you identify potential bottlenecks that need to be investigated.\n
+    The distance between each column lines shows you how long issues take to get from one state to another. 
+    Look for points where one band is growing at a faster rate than another to find bottlenecks`;
   return (
     <Card className="card">
       <div className="card-header">
         <Subheading className="card-header-text">Cumulative Flow Diagram</Subheading>
-        <Tooltip title="Help">
+        <Tooltip title={tooltipText}>
           <Button size="small" type="text" shape="circle" icon={<QuestionCircleOutlined />} />
         </Tooltip>
       </div>
