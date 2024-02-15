8c60ade3474d0b2c0f8cbb532ef4dbb69f2596c2
Kelvin <chuakid@gmail.com>
2024-02-15 12:54:14 +0800

    feat: get id from bot

--- packages/backend/src/notifications/NotificationHandler.ts
+++ packages/backend/src/notifications/NotificationHandler.ts
@@ -1,15 +1,19 @@
 import { Bot } from "grammy";
 
 
+let bot;
 export async function init() {
     // console.log(process.env.TELEGRAM_TOKEN);
     console.log("Starting bot");
 
-    const bot = new Bot(process.env.TELEGRAM_TOKEN!);
+    bot = new Bot(process.env.TELEGRAM_TOKEN!);
     // bot.catch((e) => console.log(e))
     bot.start();
     await bot.init()
     console.log("Telegram bot started", bot.botInfo.username);
 
-    bot.on("message", (ctx) => ctx.reply("test"))
+    bot.command("start", (ctx) => {
+        ctx.reply(`your id is ${ctx.message?.from.id}, please input this into your settings in trofos.`)
+    })
+
 }
