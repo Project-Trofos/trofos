fca53b492d47b4c167b2f8dd98ab74f2ce727a1c
Kelvin <chuakid@gmail.com>
2024-02-21 14:32:13 +0800

    feat: notifications for backlog updates

--- packages/backend/src/controllers/backlog.ts
+++ packages/backend/src/controllers/backlog.ts
@@ -3,10 +3,14 @@ import StatusCodes from 'http-status-codes';
 import express from 'express';
 import backlogService from '../services/backlog.service';
 import { BadRequestError, getDefaultErrorRes } from '../helpers/error';
+import { sendToProject } from '../notifications/NotificationHandler';
+import { BacklogFields } from '../helpers/types/backlog.service.types';
 
 const newBacklog = async (req: express.Request, res: express.Response) => {
   try {
     const backlog: Backlog = await backlogService.newBacklog(req.body);
+    const { projectId, summary, priority }: BacklogFields = req.body;
+    sendToProject(projectId, `Backlog added: ${summary}, priority: ${priority}`)
     return res.status(StatusCodes.OK).json(backlog);
   } catch (error) {
     return getDefaultErrorRes(error, res);
@@ -53,11 +57,13 @@ const getBacklog = async (req: express.Request, res: express.Response) => {
 
 const updateBacklog = async (req: express.Request, res: express.Response) => {
   try {
-    const { projectId, backlogId } = req.body;
+    const { projectId, backlogId, fieldToUpdate } = req.body;
     if (!projectId || !backlogId) {
       throw new BadRequestError('projectId or backlogId cannot be empty');
     }
     const backlog: Backlog = await backlogService.updateBacklog(req.body);
+    sendToProject(projectId, `Backlog ${backlogId} updated, fields updated: ${Object.keys(fieldToUpdate)}`)
+
     return res.status(StatusCodes.OK).json(backlog);
   } catch (error) {
     return getDefaultErrorRes(error, res);
@@ -71,6 +77,8 @@ const deleteBacklog = async (req: express.Request, res: express.Response) => {
       throw new BadRequestError('projectId or backlogId cannot be empty');
     }
     const backlog: Backlog = await backlogService.deleteBacklog(Number(projectId), Number(backlogId));
+    sendToProject(Number(projectId), `Backlog ${backlogId} deleted`)
+
     return res.status(StatusCodes.OK).json(backlog);
   } catch (error) {
     return getDefaultErrorRes(error, res);
