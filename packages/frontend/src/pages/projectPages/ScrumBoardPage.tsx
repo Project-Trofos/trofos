39345a577dae8e76139d0cbfef2e7c67ee8a028c
Tang Bo Kuan <e0701815@u.nus.edu>
2024-09-24 12:18:33 +0800

    T3-23 Add standup to scrum board

--- packages/frontend/src/pages/projectPages/ScrumBoardPage.tsx
+++ packages/frontend/src/pages/projectPages/ScrumBoardPage.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { Carousel, Spin } from 'antd';
 import { Sprint, useGetActiveSprintQuery, useGetSprintsByProjectIdQuery } from '../../api/sprint';
 import Container from '../../components/layouts/Container';
 import ScrumBoard from '../../components/board/ScrumBoard';
@@ -7,6 +8,13 @@ import { Heading } from '../../components/typography';
 
 import './ScrumBoardPage.css';
 import BacklogCreationModal from '../../components/modals/BacklogCreationModal';
+import { useGetStandUpHeadersByProjectIdQuery, useGetStandUpQuery } from '../../api/standup';
+import StandUpBoard from '../../components/board/StandUpBoard';
+import store from '../../app/store';
+import trofosApiSlice from '../../api';
+import useSocket from '../../api/socket/useSocket';
+import { UpdateType } from '../../api/socket/socket';
+import { useProjectIdParam } from '../../api/hooks';
 
 const getSprintHeading = (sprint: Sprint | undefined, activeSprint: Sprint | undefined): string | undefined => {
   if (!sprint) {
@@ -19,9 +27,56 @@ const getSprintHeading = (sprint: Sprint | undefined, activeSprint: Sprint | und
 };
 
 export default function ActiveScrumBoardPage(): JSX.Element {
-  const params = useParams();
-  const projectId = Number(params.projectId);
-  const { data: activeSprint } = useGetActiveSprintQuery(projectId);
+  const projectId = useProjectIdParam();
+  const { data: activeSprint, isLoading: isLoadingActiveSprint } = useGetActiveSprintQuery(projectId);
+  const { data: standUps, isLoading: isLoadingStandUpHeaders } = useGetStandUpHeadersByProjectIdQuery(projectId);
+  const [todaysStandUpId, setTodaysStandUpId] = useState<number | undefined>(undefined);
+  const { data: todaysStandUp, isLoading: isLoadingStandUp } = useGetStandUpQuery(
+    { id: todaysStandUpId! }, 
+    { skip: todaysStandUpId === undefined } // Skip the query if there's no standupId
+  );
+  
+  // Refetch active standup data upon update
+  const handleReset = useCallback(() => {
+    store.dispatch(trofosApiSlice.util.invalidateTags([{ type: 'StandUp', id: todaysStandUpId }]));
+  }, [todaysStandUpId]);
+  useSocket(UpdateType.STAND_UP_NOTES, todaysStandUpId?.toString(), handleReset);
+
+  const renderBody = () => {
+    return (
+      //<Carousel arrows draggable={false}>
+      <>
+        {activeSprint ? (
+        <div style={{ width: '100vw', height: '100vh', overflow: 'hidden' }}>
+          <ScrumBoard projectId={projectId} sprint={activeSprint} />
+        </div>
+      ) : null}
+      {todaysStandUp ? (
+        <div style={{ width: '100vw', height: '100vh', overflow: 'hidden' }}>
+          <StandUpBoard standUp={todaysStandUp} />
+        </div>
+      ) : null}
+      </>
+      //</></Carousel>
+    );
+  }
+
+  useEffect(() => {
+    if (standUps && standUps.length > 0) {
+      const isSameDay = (date1: Date, date2: Date) => (
+        date1.getFullYear() === date2.getFullYear() &&
+        date1.getMonth() === date2.getMonth() &&
+        date1.getDate() === date2.getDate()
+      );
+  
+      const todaysStandup = standUps?.find((s) => isSameDay(new Date(s.date), new Date()));
+      setTodaysStandUpId(todaysStandup?.id);
+    }
+  }, [standUps]);
+
+  if (isLoadingActiveSprint || isLoadingStandUpHeaders || isLoadingStandUp) {
+    return <Spin />;
+  }
 
   return (
     <Container noGap fullWidth className="scrum-board-container">
@@ -31,7 +86,7 @@ export default function ActiveScrumBoardPage(): JSX.Element {
         </Heading>
         {activeSprint && <BacklogCreationModal fixedSprint={activeSprint} title={'Create Backlog For This Sprint'} />}
       </div>
-      <ScrumBoard projectId={projectId} sprint={activeSprint} />
+      {renderBody()}
     </Container>
   );
 }
