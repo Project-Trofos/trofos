3d23ea45a99015381d86044c7f9dbec21809baa0
Tang Bo Kuan <e0701815@u.nus.edu>
2024-09-25 12:40:47 +0800

    T3-23 Add frontend test for new scrumboard actions

--- packages/frontend/src/components/cards/ScrumBoardCard.test.tsx
+++ packages/frontend/src/components/cards/ScrumBoardCard.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { DragDropContext } from 'react-beautiful-dnd';
@@ -7,8 +7,36 @@ import store from '../../app/store';
 import type { Backlog } from '../../api/types';
 import { ScrumBoardCard } from './ScrumBoardCard';
 import StrictModeDroppable from '../dnd/StrictModeDroppable';
+import { StandUp } from '../../api/standup';
 
 describe('ScrumBoardCard test', () => {
+  const setup = (mockBacklog: Backlog, mockStandUp?: StandUp) => {
+    const mockProjectKey = 'TEST';
+    const mockIndex = 0;
+    const mockDroppableId = '0';
+    const { baseElement, debug } = render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <DragDropContext onDragEnd={vi.fn()}>
+            <StrictModeDroppable droppableId={mockDroppableId}>
+              {(provided) => (
+                <div ref={provided.innerRef} {...provided.droppableProps}>
+                  <ScrumBoardCard
+                    backlog={mockBacklog}
+                    projectKey={mockProjectKey}
+                    id={mockIndex}
+                    index={0}
+                    standUp={mockStandUp}
+                  />
+                </div>
+              )}
+            </StrictModeDroppable>
+          </DragDropContext>
+        </BrowserRouter>
+      </Provider>,
+    );
+    return { baseElement, debug };
+  }
   const mockBacklog: Backlog = {
     backlog_id: 111,
     summary: 'A Test Summary',
@@ -32,26 +60,15 @@ describe('ScrumBoardCard test', () => {
     },
   };
 
-  const mockProjectKey = 'TEST';
-  const mockIndex = 0;
-  const mockDroppableId = '0';
-  const { baseElement } = render(
-    <Provider store={store}>
-      <BrowserRouter>
-        <DragDropContext onDragEnd={vi.fn()}>
-          <StrictModeDroppable droppableId={mockDroppableId}>
-            {(provided) => (
-              <div ref={provided.innerRef} {...provided.droppableProps}>
-                <ScrumBoardCard backlog={mockBacklog} projectKey={mockProjectKey} id={mockIndex} index={0} />
-              </div>
-            )}
-          </StrictModeDroppable>
-        </DragDropContext>
-      </BrowserRouter>
-    </Provider>,
-  );
+  const mockStandUp: StandUp = {
+    id: 1,
+    date: new Date(),
+    project_id: 903,
+    notes: [],
+  };
 
   it('renders scrum board card with correct details', () => {
+    const { baseElement } = setup(mockBacklog);
     expect(screen.getByText(mockBacklog.summary)).toBeInTheDocument();
     expect(screen.getByText(mockBacklog.type)).toBeInTheDocument();
     expect(screen.getByText(mockBacklog?.points?.toString() as string)).toBeInTheDocument();
@@ -59,4 +76,20 @@ describe('ScrumBoardCard test', () => {
     // Compare with snapshot to ensure structure remains the same
     expect(baseElement).toMatchSnapshot();
   });
+
+  it('renders scrum board card with standup options if stand up param is provided', async () => {
+    const { baseElement, debug } = setup(mockBacklog, mockStandUp);
+    expect(screen.getByText(mockBacklog.summary)).toBeInTheDocument();
+    expect(screen.getByText(mockBacklog.type)).toBeInTheDocument();
+    expect(screen.getByText(mockBacklog?.points?.toString() as string)).toBeInTheDocument();
+    const dropdownButton = screen.getByLabelText(/setting/i);
+    fireEvent.mouseOver(dropdownButton);
+    await waitFor(() => {
+      expect(screen.getByText('Mark as Done in Standup')).toBeInTheDocument();
+      expect(screen.getByText('Mark as Next in Standup')).toBeInTheDocument();
+      expect(screen.getByText('Mark as Blockers in Standup')).toBeInTheDocument();
+    });
+    // Compare with snapshot to ensure structure remains the same
+    expect(baseElement).toMatchSnapshot();
+  })
 });
