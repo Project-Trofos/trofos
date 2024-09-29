f438e9b545d565a2764614d6635634e7d678e54b
Tang Bo Kuan <e0701815@u.nus.edu>
2024-09-29 22:10:03 +0800

    T3-47 Finish basic api docs

--- packages/backend/src/routes/external/v1/project.route.external.v1.ts
+++ packages/backend/src/routes/external/v1/project.route.external.v1.ts
@@ -8,7 +8,7 @@ const router = express.Router();
 
 /**
  * @swagger
- * /external/v1/project:
+ * /v1/project:
  *  get:
  *    description: Get all projects applicable to the owner of the API key
  *    tags: [Project]
@@ -17,9 +17,45 @@ const router = express.Router();
  *    responses:
  *      200:
  *        description: Gets all projects for user that he has permissions for
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              $ref: "#/components/schemas/Project"
+ *      401:
+ *        description: Unauthorized
+ *      500:
+ *        description: Internal server error
  */
 router.get('/', hasAuthForExternalApi(Action.read_project, projectPolicy.POLICY_NAME), project.getAll);
 
+/**
+ * @swagger
+ * /v1/project/{projectId}:
+ *  get:
+ *    description: Get project by project id
+ *    tags: [Project]
+ *    produces:
+ *      - application/json
+ *    parameters:
+ *      - in: path
+ *        name: projectId
+ *        required: true
+ *        schema:
+ *          type: integer
+ *        description: ID of the project to retrieve
+ *    responses:
+ *      200:
+ *        description: Gets project details by project id
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: "#/components/schemas/Project"
+ *      401:
+ *        description: Unauthorized
+ *      500:
+ *        description: Internal server error
+ */
 router.get('/:projectId', hasAuthForProjectExternalApi(Action.read_project, projectPolicy.POLICY_NAME), project.get);
 
 export default router;
--- packages/backend/src/swagger/swagger.ts
+++ packages/backend/src/swagger/swagger.ts
@@ -2,6 +2,8 @@ import { Express } from "express";
 import swaggerJsdoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
 
+// TODO: find a better way to generate schema. Now prisma generated types cannot automatically
+// be used in swagger schemas. zenstack/openapi conversion is being experimented with
 const options: swaggerJsdoc.Options = {
   definition: {
     openapi: '3.0.0',
@@ -10,6 +12,12 @@ const options: swaggerJsdoc.Options = {
       version: '1.0.0',
       description: 'Trofos External APIs documentation',
     },
+    servers: [
+      {
+        url: process.env.FRONTEND_BASE_URL + '/api/external' || 'http://localhost:3001/api/external',
+        description: 'Server',
+      },
+    ],
     components: {
       securitySchemes: {
         ApiKeyAuth: {
@@ -17,11 +25,89 @@ const options: swaggerJsdoc.Options = {
           in: 'header',
           name: 'x-api-key',
         }
+      },
+      schemas: {
+        Project: {
+          type: 'object',
+          properties: {
+            id: {
+              type: 'integer',
+              description: 'Unique identifier for the project',
+            },
+            pname: {
+              type: 'string',
+              description: 'Name of the project',
+            },
+            description: {
+              type: 'string',
+              nullable: true,
+              description: 'Detailed description of the project',
+            },
+            course_id: {
+              type: 'integer',
+              description: 'ID of the related course',
+            },
+            course: {
+              $ref: '#/components/schemas/Course',
+              description: 'Course associated with this project',
+            },
+            public: {
+              type: 'boolean',
+              description: 'Whether the project is public or private',
+            },
+            created_at: {
+              type: 'string',
+              format: 'date-time',
+              description: 'Creation timestamp of the project',
+            },
+            updated_at: {
+              type: 'string',
+              format: 'date-time',
+              description: 'Last updated timestamp of the project',
+            },
+            backlog_counter: {
+              type: 'integer',
+              description: 'The count of items in the backlog',
+            },
+          },
+          required: ['id', 'pname', 'course_id', 'public', 'created_at', 'backlog_counter'],
+        },
+        Course: {
+          type: 'object',
+          properties: {
+            id: {
+              type: 'integer',
+              description: 'Unique identifier for the course',
+            },
+            name: {
+              type: 'string',
+              description: 'Name of the course',
+            },
+            description: {
+              type: 'string',
+              nullable: true,
+              description: 'Description of the course',
+            },
+            created_at: {
+              type: 'string',
+              format: 'date-time',
+              description: 'Creation timestamp of the course',
+            },
+            updated_at: {
+              type: 'string',
+              format: 'date-time',
+              description: 'Last updated timestamp of the course',
+            },
+          },
+          required: ['id', 'name', 'created_at'],
+        },
       }
     },
-    security: {
-      ApiKeyAuth: []
-    }
+    security: [
+      {
+        ApiKeyAuth: []
+      }
+    ]
   },
   apis: ['./src/routes/external/**/*.ts']
 }
