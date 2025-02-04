c6cdab4b782186a20dfb7228f5c075a7bcb60b4b
Tang Bo Kuan <e0701815@u.nus.edu>
2025-02-04 21:05:04 +0800

    Add feature flag middleware

--- packages/backend/src/routes/ai.route.ts
+++ packages/backend/src/routes/ai.route.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import { hasAuth } from '../middleware/auth.middleware';
 import ai from '../controllers/ai'
+import { checkFeatureFlag } from '../middleware/feature_flag.middleware';
+import { Feature } from '@prisma/client';
 
 const router = express.Router();
 
-router.post('/userGuideQuery', hasAuth(null, null), ai.answerUserGuideQuery);
+router.post('/userGuideQuery', hasAuth(null, null), checkFeatureFlag(Feature.user_guide_copilot), ai.answerUserGuideQuery);
 
 export default router;
