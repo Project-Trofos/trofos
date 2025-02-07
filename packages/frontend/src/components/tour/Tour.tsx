196ee59ac7fe4c8e01c0d3e5c72ec76df165a91f
thamruicong <ruicongtham@gmail.com>
2025-02-07 13:37:03 +0800

    add revisit tour description

--- packages/frontend/src/components/tour/Tour.tsx
+++ packages/frontend/src/components/tour/Tour.tsx
@@ -8,18 +8,13 @@ import { ADMIN_ROLE_ID, STUDENT_ROLE_ID, FACULTY_ROLE_ID } from '../../api/role'
 import { getStudentSteps, getFacultySteps, getAdminSteps } from './TourSteps';
 import { useTourContext } from './TourProvider';
 
-const TOUR_STEP_STORAGE_KEY = 'currentTourStep';
-
 export default function TourComponent(): JSX.Element {
   const { data: userInfo } = useGetUserInfoQuery();
   const { isTourOpen, startTour, endTour } = useTourContext();
   const [updateUserInfo] = useUpdateUserInfoMutation();
   const navigate = useNavigate();
 
-  const [currentStep, setCurrentStep] = useState<number>(() => {
-    const savedStep = localStorage.getItem(TOUR_STEP_STORAGE_KEY); // Get the saved step from local storage if it exists
-    return savedStep ? parseInt(savedStep, 10) : 0;
-  });
+  const [currentStep, setCurrentStep] = useState<number>(0);
   const isFirstRender = useRef(true);
 
   const roleSteps = useMemo<TourProps['steps']>(() => {
@@ -45,11 +40,6 @@ export default function TourComponent(): JSX.Element {
     }
   }, [userInfo, navigate]);
 
-  const handleTourStepChange = (step: number) => {
-    setCurrentStep(step);
-    localStorage.setItem(TOUR_STEP_STORAGE_KEY, step.toString());
-  };
-
   const handleTourClose = async () => {
     if (!userInfo) return;
 
@@ -60,7 +50,7 @@ export default function TourComponent(): JSX.Element {
         userId: userInfo.userId,
         hasCompletedTour: true,
       }).unwrap();
-      localStorage.removeItem(TOUR_STEP_STORAGE_KEY);
+      setCurrentStep(0);
     } catch (error) {
       console.error(getErrorMessage(error));
       message.error('Failed to update tour completion status.');
@@ -72,7 +62,7 @@ export default function TourComponent(): JSX.Element {
       open={isTourOpen}
       onClose={handleTourClose}
       current={currentStep}
-      onChange={handleTourStepChange}
+      onChange={setCurrentStep}
       steps={roleSteps}
       disabledInteraction={true}
       mask={false}
--- packages/frontend/src/components/tour/TourButton.tsx
+++ packages/frontend/src/components/tour/TourButton.tsx
@@ -1,8 +1,13 @@
 import { Button } from 'antd';
 import { useTourContext } from './TourProvider';
+import { STEP_PROP, StepTarget } from './TourSteps';
 
 export default function TourButton() {
   const { startTour } = useTourContext();
 
-  return <Button onClick={startTour}>Take a Quick Tour</Button>;
+  return (
+    <Button {...{ [STEP_PROP]: StepTarget.START_TOUR_BUTTON }} onClick={startTour}>
+      Take a Quick Tour
+    </Button>
+  );
 }
