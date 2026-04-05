d895954e6e4cd24c3ac6c0e8fc41913676385988
livvis <livvisim@gmail.com>
2026-04-06 07:39:29 +0800

    Updated pino configs for viewing in grafana

--- packages/backend/src/logger/config.ts
+++ packages/backend/src/logger/config.ts
@@ -8,6 +8,21 @@ interface LoggerConfig {
   test: LoggerOptions;
 }
 
+const timestampFormatter = new Intl.DateTimeFormat('en-CA', {
+  year: 'numeric',
+  month: '2-digit',
+  day: '2-digit',
+  hour: '2-digit',
+  minute: '2-digit',
+  second: '2-digit',
+  timeZoneName: 'shortOffset',
+  hour12: false,
+});
+
+function formatTimestamp() {
+  return `,"timestamp":"${timestampFormatter.format(new Date()).replace(', ', ' ')}"`;
+}
+
 const baseConfig: Record<string, string> = {
   service: 'backend',
   env: process.env.NODE_ENV || ENVIRONMENTS.DEVELOPMENT,
@@ -17,6 +32,7 @@ const defaultConfig: LoggerConfig = {
   development: {
     level: 'debug',
     base: null,
+    timestamp: formatTimestamp,
     transport: {
       targets: [
         {
@@ -44,18 +60,18 @@ const defaultConfig: LoggerConfig = {
   staging: {
     level: 'info',
     base: baseConfig,
+    timestamp: formatTimestamp,
     formatters: {
       level: (label) => ({ level: label }),
     },
-    timestamp: () => `,"timestamp":"${new Date().toISOString()}"`,
   },
   production: {
     level: 'info',
     base: baseConfig,
+    timestamp: formatTimestamp,
     formatters: {
       level: (label) => ({ level: label }),
     },
-    timestamp: () => `,"timestamp":"${new Date().toISOString()}"`,
   },
   test: {
     level: 'silent',
