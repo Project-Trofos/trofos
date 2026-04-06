367f2c351bc4238471ea731afaffb2407b944cc9
livvis <livvisim@gmail.com>
2026-04-06 08:50:56 +0800

    Added node process event logging

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,14 +1,43 @@
+import process from 'node:process';
 import cookieParser from 'cookie-parser';
 import { Server, Socket } from 'socket.io';
 import app, { corsOptions, port } from './server';
 import { init } from './services/socket.service';
 import { init as initBot} from './notifications/NotificationHandler'
 import { initCompleteInsightSub } from './services/aiInsight.service';
+import { getLogger } from './logger/loggerProvider';
+
+const logger = getLogger();
 
 const server = app.listen(port, () => {
   // eslint-disable-next-line no-console
-  console.log(`App listening at port ${port}.`);
+  logger.info(`App listening at port ${port}.`);
 });
+
+process.on('warning', (warning: Error) => {
+  logger.warn({ err: warning }, 'Node process warning');
+});
+
+process.on('exit', (code: number) => {
+  logger.info({ exit_code: code }, `Process exited with code ${code}`);
+});
+
+process.on('unhandledRejection', (reason: unknown) => {
+  logger.fatal({ err: reason }, 'Unhandled Rejection detected. Shutting down...');
+
+  server.close(() => process.exit(1));
+  setTimeout(() => process.abort(), 1000).unref();
+  process.exit(1);
+});
+
+process.on('uncaughtException', (err: Error, origin: NodeJS.UncaughtExceptionOrigin) => {
+  logger.fatal({ err, origin }, 'Uncaught Exception detected. Shutting down...');
+
+  server.close(() => process.exit(1));
+  setTimeout(() => process.abort(), 1000).unref();
+  process.exit(1);
+});
+
 
 const wrap = (middleware: any) => (socket: Socket, next: any) => middleware(socket.request, {}, next);
 
